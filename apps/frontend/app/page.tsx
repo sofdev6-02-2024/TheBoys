de53dd153464b790f29eeca3383348709184db47
gaston <gaston.gutierrez.9964359@usalesiana.edu.bo>
2024-10-25 09:39:11 -0400

    resolution

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -2,17 +2,16 @@ import Header from "../app/HomePage/Header";
 import Footer from "./HomePage/Footer";
 import HomePage from "./HomePage/HomePage";
 
-
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black flex flex-col">
-      <Header />
-      <main className="flex-grow">
-       
-        <HomePage/>
-        
-      </main>
-      <Footer />
+    <div className="min-h-screen bg-[#28292E] flex flex-col items-center">
+      <div className="max-w-[1440px] w-full flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <HomePage />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
