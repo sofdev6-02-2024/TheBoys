cbecb2f4f3a151188700b75daf7388ed52530ce9
gaston <gaston.gutierrez.9964359@usalesiana.edu.bo>
2024-10-25 07:52:09 -0400

    delete imports

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "../app/HomePage/Header";
 import Footer from "./HomePage/Footer";
 import HomePage from "./HomePage/HomePage";
-import Link from "next/link";
+
 
 export default function Home() {
   return (
