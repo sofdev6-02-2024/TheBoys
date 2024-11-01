8edc5fbbe9e7b284aec14b52022f89462ec4e2f2
Rodrigo Esprella <hairaxelpoderoso@gmail.com>
2024-11-01 13:03:58 -0400

    style login page

--- apps/frontend/app/LoginPage/page.tsx
+++ apps/frontend/app/LoginPage/page.tsx
@@ -31,30 +31,30 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-[#28292E]">
-            <div className="flex flex-col w-[500px] h-[430px] items-center justify-center">
-                <div className="flex flex-col items-center justify-center ">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#28292E] px-4">
+            <div className="flex flex-col items-center p-5 rounded-lg w-full max-w-xs md:max-w-md lg:max-w-lg">
+                <div className="flex flex-col items-center mb-5">
                     <p className="text-2xl font-bold text-[#FEFFFF] text-[36px]">
                         BODY BOOST
                     </p>
-                    <Image src={logo} alt="Logo" width={70} height={70} className="mt-3"/>
+                    <Image src={logo} alt="Logo" width={70} height={70} className="mt-3" />
                 </div>
-                <form className="flex flex-col mt-7 w-[470px]" onSubmit={handleSubmit}>
-                    <label className="text-[#FEFFFF] mb-2 text-left">Email:</label>
+                <form className="flex flex-col w-full max-w-[295px] md:max-w-[470px]" onSubmit={handleSubmit}>
+                    <label className="text-[#FEFFFF] mb-2 text-left text-[12px]">Email:</label>
                     <input 
                         type="text"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className="p-2 border border-[#BDBDBD] rounded-lg bg-[#28292E] w-full h-[50px]"
+                        className="p-2 border border-[#BDBDBD] rounded-lg bg-[#28292E] w-full h-[50px] text-white"
                     />
-                    <label className="text-[#FEFFFF] mt-7 mb-2 text-left">Password:</label>
+                    <label className="text-[#FEFFFF] mt-7 mb-2 text-left text-[12px]">Password:</label>
                     <input 
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        className="p-2 border border-[#BDBDBD] rounded-lg bg-[#28292E] w-full h-[50px]"
+                        className="p-2 border border-[#BDBDBD] rounded-lg bg-[#28292E] w-full h-[50px] text-white"
                     />
-                    <button type="submit" className="bg-[#BA474A] text-white p-2 mt-7 rounded-lg w-full h-[48px]">
+                    <button type="submit" className="bg-[#BA474A] text-white text-[24px] p-2 mt-7 rounded-lg w-full h-[48px]">
                         Login
                     </button>
                 </form>
