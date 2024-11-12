f9d5876a0b7f572ee2a31b3cf09db15a5fbb3f58
JheremyTancara <jheremykay777@gmail.com>
2024-11-12 12:17:36 -0400

    fix: improve imports of user

--- apps/auth-service/src/users/users.module.ts
+++ apps/auth-service/src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
