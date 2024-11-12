53d0fde97eb6bfb8097f0ac4b06d70e15bb94919
JheremyTancara <jheremykay777@gmail.com>
2024-11-12 12:22:18 -0400

    feat: add users controller

--- infraestructure/api-gateway/src/users/users.controller.ts
+++ infraestructure/api-gateway/src/users/users.controller.ts
@@ -12,39 +12,39 @@ import {
   import { Observable } from 'rxjs';
 import { ObjectId } from 'mongodb';
   
-  @Controller('users')
-  export class UsersController {
-    constructor(
-      @Inject('AUTH_SERVICE')
-      private readonly authService: ClientProxy,
-    ) {}
-  
-    @Get()
-    findAll(): Observable<any> {
-      return this.authService.send('findAllUsers', {});
-    }
-  
-    @Get(':id')
-    findOne(@Param('id') id: string): Observable<any> { 
-      return this.authService.send('findOneUser', id);
-    }
-  
-    @Post()
-    create(@Body() createUserDto: any): Observable<any> {
-      return this.authService.send('createUser', createUserDto);
-    }
-  
-    @Put(':id')
-    update(@Body() updateUserDto: any, @Param('id') id: ObjectId): Observable<any> {  
-      return this.authService.send('updateUser', {
-        id: id,
-        updateUserDto,
-      });
-    }
-  
-    @Delete(':id')
-    remove(@Param('id') id: string): Observable<any> {  
-      return this.authService.send('removeUser', id);
-    }
+@Controller('users')
+export class UsersController {
+  constructor(
+    @Inject('AUTH_SERVICE')
+    private readonly authService: ClientProxy,
+  ) {}
+
+  @Get()
+  findAll(): Observable<any> {
+    return this.authService.send('findAllUsers', {});
   }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<any> { 
+    return this.authService.send('findOneUser', id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: any): Observable<any> {
+    return this.authService.send('createUser', createUserDto);
+  }
+
+  @Put(':id')
+  update(@Body() updateUserDto: any, @Param('id') id: ObjectId): Observable<any> {  
+    return this.authService.send('updateUser', {
+      id: id,
+      updateUserDto,
+    });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Observable<any> {  
+    return this.authService.send('removeUser', id);
+  }
+}
   
