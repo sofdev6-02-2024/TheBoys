fd17bca685671325e37a532b7ff5d4c1cb93cdf6
AndrePzDn <andreperezduran@gmail.com>
2024-11-21 17:38:58 -0400

    feat: add keycloak auth provider

--- infraestructure/api-gateway/src/app.module.ts
+++ infraestructure/api-gateway/src/app.module.ts
@@ -1,8 +1,40 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EngageHubModule } from './engage-hub/engage-hub.module';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, EngageHubModule],
+  imports: [
+    ConfigModule.forRoot(),
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://host.docker.internal:8080',
+      realm: 'body-boost',
+      clientId: 'nextjs',
+      secret: 'GwiCZfxus2kTB6e14glNJDAf63VjoNKv',
+    }),
+    UsersModule,
+    EngageHubModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
