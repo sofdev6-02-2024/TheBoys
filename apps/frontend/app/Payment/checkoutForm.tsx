1a33544ec358b9fe52919aa1f524a883aa0b70ec
AndrePzDn <andreperezduran@gmail.com>
2024-11-29 03:09:59 -0400

    feat: implement basic payment page

--- apps/frontend/app/Payment/checkoutForm.tsx
+++ apps/frontend/app/Payment/checkoutForm.tsx
@@ -1,138 +1,55 @@
-"use client"
 import {
-  CardCvcElement,
-  CardExpiryElement,
-  CardNumberElement,
+  PaymentElement,
   useElements,
   useStripe,
-} from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
-import { toast } from 'sonner';
+} from "@stripe/react-stripe-js";
+import { toast } from "sonner";
+import Button from "../components/Button";
 
 function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
-  const [loading, setLoading] = useState(false);
-  const [cardNumberComplete, setCardNumberComplete] = useState(false);
-  const [expiryComplete, setExpiryComplete] = useState(false);
-  const [cvcComplete, setCvcComplete] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
+  const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    const cardElement = elements?.getElement(CardNumberElement);
-    if (!stripe || !elements || !cardElement) {
-      toast.error('Stripe has not loaded yet.');
-      setLoading(false);
+    if (!stripe || !elements) {
       return;
     }
 
-    const { error: cardError, paymentMethod } =
-      await stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
-      });
+    const result = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: "https://example.com/order/123/complete",
+      },
+    });
 
-    if (cardError) {
-      toast.error(cardError.message || 'An error occurred while creating the payment method.');
-      setLoading(false);
-      return;
+    console.log(result)
+
+    if (result.error) {
+      toast.error("A processing error occurred.");
+    } else {
+      toast.success("Your payment was processed")
     }
-
-    const { id } = paymentMethod;
-    try {
-      const response = await fetch('http://localhost:3000/api/stripe', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id,
-          amount: 1000, // Amount in cents
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Payment failed');
-      }
-
-      const data = await response.json();
-      console.log(data);
-      cardElement.clear();
-      toast.success('Payment successful! 🎉');
-    } catch (error) {
-      toast.error('Payment failed. Please try again.');
-      console.error(error);
-    }
-
-    setLoading(false);
   };
 
-  const handleCardNumberChange = (event: { complete: boolean }) =>
-    setCardNumberComplete(event.complete);
-  const handleExpiryChange = (event: { complete: boolean }) =>
-    setExpiryComplete(event.complete);
-  const handleCvcChange = (event: { complete: boolean }) =>
-    setCvcComplete(event.complete);
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md">
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="cardNumber">
-            Card Number
-          </label>
-          <CardNumberElement
-            onChange={handleCardNumberChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+    <div className="flex justify-center items-center h-full p-4 sm:p-0">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col bg-primary-hover shadow-md rounded-xl px-8 pt-6 pb-8 mb-4 w-full max-w-md gap-4"
+      >
+        <PaymentElement />
+        <div className="flex items-center justify-between">
+          <Button
+            backgroundColor="secondary"
+            type="submit"
+            disabled={!stripe}
+            className="w-full"
+          >
+            Submit Payment
+          </Button>
         </div>
-        <div className="mb-4 sm:flex sm:space-x-4">
-          <div className="mb-4 sm:mb-0 sm:w-1/2">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="expiryDate">
-              Expiration Date
-            </label>
-            <CardExpiryElement
-              onChange={handleExpiryChange}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-          <div className="sm:w-1/2">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="cvc">
-              CVC
-            </label>
-            <CardCvcElement
-              onChange={handleCvcChange}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-        </div>
-        {loading && (
-          <div className="mb-4 text-center">
-            <div className="spinner-border text-primary" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
-          </div>
-        )}
-        {!loading && (
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              disabled={
-                !stripe ||
-                loading ||
-                !(cardNumberComplete && expiryComplete && cvcComplete)
-              }
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Submit Payment
-            </button>
-            {(cardNumberComplete && expiryComplete && cvcComplete) && (
-              <span className="text-green-500 font-bold">Form complete</span>
-            )}
-          </div>
-        )}
       </form>
     </div>
   );
